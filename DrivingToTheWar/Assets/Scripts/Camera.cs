79af87e11c2705ef9b1fe15a32fcd5b66efe12db
yukiiris <25852469@qq.com>
2017-08-07 10:58:58 +0800

    第一行脚本

---
+++ DrivingToTheWar/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	private int angle = 90;
+	private float speed = 130f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void rotate()
+	{
+
+	}
+}
