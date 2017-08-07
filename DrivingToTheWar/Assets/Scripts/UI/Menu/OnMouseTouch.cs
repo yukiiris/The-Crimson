76787096817388a5fc5ec6897fed5043380538b9
LLH <837233905@qq.com>
2017-08-07 10:47:16 +0800

    new storage

---
+++ DrivingToTheWar/Assets/Scripts/UI/Menu/OnMouseTouch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseTouch : MonoBehaviour {
+
+	public bool ifMouseOver = false;
+	public bool ifMouseLeave = false;
+
+	void OnMouseEnter () {
+		ifMouseOver = true;
+	}
+
+	void OnMouseExit () {
+		ifMouseLeave = true;
+	}
+}
