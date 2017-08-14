0144d2fa779302e933f6d15bcb7069bb2266c353
moltres <25852469@qq.com>
2017-08-14 13:38:55 +0800

    c#文件读写

--- DrivingToTheWar/DrivingToTheWar/Assets/Scripts/Archive/ArchiveManager.cs
+++ DrivingToTheWar/DrivingToTheWar/Assets/Scripts/Archive/ArchiveManager.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 public class ArchiveManager : MonoBehaviour {
 
+	public static string path;
+
 	private void Awake()
 	{
 		save();
@@ -21,30 +23,51 @@ public class ArchiveManager : MonoBehaviour {
 		
 	}
 
-
-	public static void save()
+	public static void load()
 	{
-		byte[] byData = new byte[200];
-		char[] charData = new char[200];
+		byte[] byData = new byte[1024];
+		char[] charData = new char[1024];
 
 		try
 		{
-			FileStream aFile = new FileStream("ArchiveManager.cs", FileMode.Open);
-			aFile.Seek(135, SeekOrigin.Begin);
+			FileStream aFile = new FileStream(path, FileMode.Open);
 			aFile.Read(byData, 0, 200);
 		}
 		catch (IOException e)
 		{
 			print("An IO exception has been thrown!");
 			print(e.ToString());
-	
+
 			return;
 		}
 
 		Decoder d = Encoding.UTF8.GetDecoder();
-		d.GetChars(byData, 0, byData.Length, charData, 0);
+		string myStr = System.Text.Encoding.UTF8.GetString(byData);
+		print(myStr);
+	}
 
-		print(charData);
+	public static void save()
+	{
+		byte[] byData;
+		char[] charData;
 
+		try
+		{
+			FileStream aFile = new FileStream(path, FileMode.Create);
+			charData = "My pink half of the drainpipe.".ToCharArray();
+			byData = new byte[charData.Length];
+			Encoder e = Encoding.UTF8.GetEncoder();
+			e.GetBytes(charData, 0, charData.Length, byData, 0, true);
+
+			// Move file pointer to beginning of file.
+			aFile.Seek(0, SeekOrigin.Begin);
+			aFile.Write(byData, 0, byData.Length);
+		}
+		catch (IOException ex)
+		{
+			Console.WriteLine("An IO exception has been thrown!");
+			Console.WriteLine(ex.ToString());
+			return;
+		}
 	}
 }
